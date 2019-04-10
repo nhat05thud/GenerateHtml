1be29be6917abcdb0e466cad59bf76f89cabd6d0
Nhat05 <nguyenducnhat05@gmail.com>
2019-04-10 18:06:39 +0700

    init

---
+++ GenerateHtml/App_Code/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GenerateHtml
+{
+    public class Utils
+    {
+        public static readonly string LayoutStyleSheet = "##stylesheet#";
+        public static readonly string LayoutHeader = "##header#";
+        public static readonly string LayoutContent = "##content#";
+        public static readonly string LayoutFooter = "##footer#";
+        public static readonly string LayoutScript = "##script#";
+    }
+}
---
+++ GenerateHtml/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web.Mvc;
+
+namespace GenerateHtml.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChooseOption()
+        {
+            var styleSheets = "<link href='css/owl.carousel.min.css' /><link href='css/fonts.css' />";
+            var strContent = "<div class='wrap-components'><div class='form-group'><input type = 'text' class='form-control' placeholder='Website Name' name='WebsiteName' value='' /></div></div>";
+            var textFile = ReplaceTextFile(styleSheets, strContent);
+            var fileName = "index";
+            using (var stream = new MemoryStream())
+            {
+                var writer = new StreamWriter(stream);
+                writer.Write(textFile);
+                writer.Flush();
+                var subPath = "/_output";
+                var exists = System.IO.Directory.Exists(Server.MapPath(subPath));
+                if (!exists)
+                {
+                    System.IO.Directory.CreateDirectory(Server.MapPath(subPath));
+                }
+                var path = subPath + "/" + fileName + ".html";
+                stream.Seek(0, SeekOrigin.Begin);
+                using (var fs = new FileStream(Server.MapPath(path), FileMode.OpenOrCreate))
+                {
+                    stream.WriteTo(fs);
+                    fs.Flush();
+                }
+            }
+            return null;
+        }
+        private string ReplaceTextFile(string styleSheets, string strContent)
+        {
+            try
+            {
+                var fileStream = new FileStream(Server.MapPath(@"/CurrentLayout/layout.html"), FileMode.Open, FileAccess.Read);
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    var textFile = streamReader.ReadToEnd();
+                    textFile = textFile.Replace(Utils.LayoutStyleSheet, styleSheets);
+                    textFile = textFile.Replace(Utils.LayoutContent, strContent);
+                    return textFile;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
